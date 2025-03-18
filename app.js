d5e83c43d5a364b0dd58fb43c74e2d6c73d697a5
Fernando Cordoba <efecba11@gmail.com>
2025-03-18 19:28:12 -0300

    1.0

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ function actualizarMenu() {
             `;
         });
         menu.innerHTML += `<p>Total: $${calcularTotalCarrito()}</p>
-        <button class="text-focus-in"><a href="../pages/carrito.html">Carrito</a></button>`;
+        <button class="text-focus-in"><a href="../JS.Segunda_Entrega/pages/carrito.html">Carrito</a></button>`;
     } else {
         menu.innerHTML = '<p>El carrito está vacío</p>';
     }
